a8f3c67e07f8f058bd7d5ece09a059581a975a20
Turbotard <turbotard9@gmail.com>
2023-03-16 10:14:26 +0100

    fixed update bugged

--- Assets/Scripts/ennemy/EnnemyAI.cs
+++ Assets/Scripts/ennemy/EnnemyAI.cs
@@ -1,12 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class EnnemyAI : MonoBehaviour
 {
 
     // Agent de navigation
     private UnityEngine.AI.NavMeshAgent agent;
+
+    [SerializeField]
+    private UnityEvent m_OnTouch;
+
+    [SerializeField]
+    private int TaDistanceQueTuConsidèreAssezPetite = 3;
 
 
     public Transform target;
@@ -28,4 +35,14 @@ public class EnnemyAI : MonoBehaviour
             agent.destination = player.transform.position;
         }
     }
+    private void FixedUpdate()
+    {
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        GameObject enemy = GameObject.FindGameObjectWithTag("Ghoul");
+        
+
+        if (Vector3.Distance(player.transform.position, enemy.transform.position) < TaDistanceQueTuConsidèreAssezPetite)
+            //Debug.Log("touché");
+            m_OnTouch.Invoke();
+     }
 }
